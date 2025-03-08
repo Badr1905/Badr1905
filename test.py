bed75ea918410638d41256f5113873b85f9823af
Badr <157146413+Badr1905@users.noreply.github.com>
2025-03-08 15:47:14 +0000

    python test
    This is my first practice basic knowledge in -Python-

---
+++ test.py
@@ -0,0 +1,16 @@
+print  ("hello world")
+message = "hello eric would you like to learn some pytho today?"
+print (message)
+print (message.title())
+print (message.upper())
+print (message.lower())
+print ("Albert Enstein once said, 'Aperson who never made a mistake never tried anything new '")
+famose_preson = "albert enstein"
+message =  ' once said, "A person who never made a mistake never tried anythig new '
+print (f"{message}languge:\n\tPython\n\tC\n\tJava")
+message =" python "
+print (message.rstrip())
+print (message.lstrip())
+print (message.strip())
+filname='python-nots.txt'
+print (filname.removeprefix('python-'))
